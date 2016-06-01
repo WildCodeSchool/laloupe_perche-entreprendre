e23cbd3203f19f19c44e50dd356b6688d43fbbdb
Kikucheeseful <gregorybizet@gmail.com>
2016-06-01 17:13:40 +0200

    tiles animation ok!

--- public/js/specJquery.js
+++ public/js/specJquery.js
@@ -10,7 +10,8 @@ $(window).load(function() {
         $('.controlCreateActivity').click(function() {
             $('body').css({
                 padding: '0px',
-                overflowX: 'hidden'
+                overflowX: 'hidden',
+                overflowY: 'scroll'
             });
             $('.container').css({
                 marginLeft: '0px',
@@ -33,15 +34,16 @@ $(window).load(function() {
                     $('.littletitle').css({
                         marginTop: '-50px'
                     });
-                    $('.icones').hide();
                     $('.form-pop').animate({
                         height: '100%',
-                        paddingTop: '50px',
                         opacity: '1'
                     }, 'slow');
                 }
             });
             $('.tile').not('.controlCreateActivity').css({
+                display : 'none'
+            });
+            $('.footer').not('.controlCreateActivity').css({
                 display: 'none'
             });
             smallCreateTile()
@@ -53,38 +55,46 @@ $(window).load(function() {
     //Rétrecissement tuile "créer activité/contact"
     function smallCreateTile() {
         $('.close-button').click(function() {
-            $('body').css({
-                padding: '',
-                overflowX: 'visible'
-            });
-            $('.container').css({
-                marginLeft: '',
-                marginRight: '',
-                marginTop: '',
-                width: '',
-                height: ''
-            });
             $('.form-pop').animate({
                 height: '150px'
             }, {
                 duration: 200,
                 complete: function() {
+
                     $('.form-pop').css({
                         opacity: '0',
                     });
                     $('.controlCreateActivity').animate({
-                        height: '250px',
-                        width: '800px'
+                        width: '0px',
+                        height: '0px',
+                        opacity: '0'
                     }, {
                         duration: 400,
                         complete: function() {
-                            $('.tile').show();
+                            $('body').css({
+                                padding: ''
+                            });
+
+                            $('.container').css({
+                                marginLeft: '',
+                                marginRight: '',
+                                marginTop: '',
+                                width: '',
+                                height: ''
+                            });
+
+                            $(this).animate({
+                                height: '250px',
+                                width: '800px',
+                                opacity: '1'
+                            }, 200);
+                            $('.tile').fadeIn(200);
+                            $('.footer').fadeIn(200);
                         }
                     });
-
                 }
             });
-
+            bigCreateTile();
         });
     }
     // Fin fonction  Rétrécissement tuile "créer activité/contact"
@@ -129,7 +139,12 @@ $(window).load(function() {
 
                 }
             });
+
+
             $('.tile').not(this).css({
+                display: 'none'
+            });
+            $('.footer').css({
                 display: 'none'
             });
             smallAgendaTile();
@@ -139,41 +154,65 @@ $(window).load(function() {
 
 
     //Rétrecissement tuile "Agenda"
-  function smallAgendaTile() {
+    function smallAgendaTile() {
 
         $('.close-button').click(function() {
-            $('body').css({
-                padding: '',
-                overflowX: 'visible'
-            });
-            $('.container').css({
-                marginLeft: '',
-                marginRight: '',
-                marginTop: '',
-                width: '',
-                height: ''
-            });
             $('.form-pop-agenda').animate({
                 height: '150px'
             }, {
                 duration: 200,
                 complete: function() {
+
                     $('.form-pop-agenda').css({
                         opacity: '0',
                     });
                     $('.controlMyAgenda').animate({
-                        height: '250px',
-                        width: '550px'
+                        width: '0px',
+                        height: '0px',
+                        opacity: '0'
                     }, {
                         duration: 400,
                         complete: function() {
+                            $('body').css({
+                                padding: ''
+                            });
+
+                            $('.container').css({
+                                marginLeft: '',
+                                marginRight: '',
+                                marginTop: '',
+                                width: '',
+                                height: ''
+                            });
+                            $('.tile').fadeIn(200);
+                            $('.footer').fadeIn(200)
+
+                            $('.footer').animate({
+
+                            }, {
+                              duration: 250,
+                              complete: function() {
+                                $('.controlMyAgenda').animate({
+                                    height: '250px',
+                                    width: '550px',
+                                    opacity: '1'
+                                }, 400);
+                              }
+                            });
+
+
+                            /*$(this).animate({
+                                height: '250px',
+                                width: '550px',
+                                opacity: '1'
+                            }, 200);
                             $('.tile').show();
+                            $('.footer').show();*/
                         }
                     });
-
                 }
             });
-              bigAgendaTile();
+            bigAgendaTile();
         });
     }
 
@@ -223,7 +262,9 @@ $(window).load(function() {
             $('.tile').not(this).css({
                 display: 'none'
             });
-
+            $('.footer').css({
+                display: 'none'
+            });
             smallSocietyTile();
         });
 
@@ -235,140 +276,158 @@ $(window).load(function() {
     //Rétrecissement tuile "society"
     function smallSocietyTile() {
 
-        $('.close-button').click(function() {
+      $('.close-button').click(function() {
+          $('.form-pop-society').animate({
+              height: '150px'
+          }, {
+              duration: 200,
+              complete: function() {
+
+                  $('.form-pop-society').css({
+                      opacity: '0',
+                  });
+                  $('.controlBuySociety').animate({
+                      width: '0px',
+                      height: '0px',
+                      opacity: '0'
+                  }, {
+                      duration: 400,
+                      complete: function() {
+                          $('body').css({
+                              padding: ''
+                          });
+
+                          $('.container').css({
+                              marginLeft: '',
+                              marginRight: '',
+                              marginTop: '',
+                              width: '',
+                              height: ''
+                          });
+
+                          $(this).animate({
+                              height: '250px',
+                              width: '550px',
+                              opacity: '1'
+                          }, 300);
+                          $('.tile').fadeIn(200);
+                          $('.footer').fadeIn(200);
+                      }
+                  });
+              }
+          });
+          bigSocietyTile();
+      });
+    }
+
+    // Fin fonction  Rétrécissement tuile "society"
+
+
+
+
+    bigMovePercheTile()
+
+    //Grossissement tuile "installez-vous dans le Perche"
+    function bigMovePercheTile() {
+        $('.movePerche').click(function() {
             $('body').css({
-                padding: '',
-                overflowX: 'visible'
+                padding: '0px',
+                overflowX: 'hidden'
             });
             $('.container').css({
-                marginLeft: '',
-                marginRight: '',
-                marginTop: '',
-                width: '',
-                height: ''
-            });
-            $('.form-pop-society').animate({
-                height: '150px'
+                marginLeft: '0px',
+                marginRight: '0px',
+                marginTop: '0px',
+                width: '102%',
+                height: '100%'
+            })
+            $(this).animate({
+                marginLeft: '0px',
+                marginTop: '0px',
+                width: '100%',
+                height: '100px',
+                opacity: '1',
+                zIndex: '999',
+                transition: 'easeOut'
             }, {
                 duration: 200,
                 complete: function() {
-                    $('.form-pop-society').css({
-                        opacity: '0',
+                    $('.littletitle').css({
+                        marginTop: '-50px'
                     });
-                    $('.controlBuySociety').animate({
-                        height: '250px',
-                        width: '550px'
-                    }, {
-                        duration: 400,
-                        complete: function() {
-                            $('.tile').show();
-                        }
-                    });
+                    $('.form-pop-lePerche').animate({
+                        height: '100%',
+                        paddingTop: '50px',
+                        opacity: '1',
+                        zIndex: '998'
+                    }, 'slow');
 
                 }
             });
-              bigSocietyTile();
+            $('.tile').not(this).css({
+                display: 'none'
+            });
+
+
+            $('.footer').css({
+                display: 'none'
+            });
+
+            smallMovePercheTile();
         });
+
     }
-
-// Fin fonction  Rétrécissement tuile "society"
-
+    // Fin fonction  Aggrandissement tuile "installez-vous dans le Perche"
 
 
 
-bigMovePercheTile()
+    //Rétrecissement tuile "installez-vous dans le Perche"
+    function smallMovePercheTile() {
+      $('.close-button').click(function() {
+          $('.form-pop-lePerche').animate({
+              height: '150px'
+          }, {
+              duration: 200,
+              complete: function() {
 
-//Grossissement tuile "installez-vous dans le Perche"
-function bigMovePercheTile() {
-  $('.movePerche').click(function() {
-      $('body').css({
-          padding: '0px',
-          overflowX: 'hidden'
+                  $('.form-pop-lePerche').css({
+                      opacity: '0',
+                  });
+                  $('.movePerche').animate({
+                      width: '0px',
+                      height: '0px',
+                      opacity: '0'
+                  }, {
+                      duration: 400,
+                      complete: function() {
+                          $('body').css({
+                              padding: ''
+                          });
+
+                          $('.container').css({
+                              marginLeft: '',
+                              marginRight: '',
+                              marginTop: '',
+                              width: '',
+                              height: ''
+                          });
+
+                          $(this).animate({
+                              height: '250px',
+                              width: '700px',
+                              opacity: '1'
+                          }, 300);
+                          $('.tile').fadeIn(200);
+                          $('.footer').fadeIn(200);
+                      }
+                  });
+              }
+          });
+          bigMovePercheTile();
       });
-      $('.container').css({
-          marginLeft: '0px',
-          marginRight: '0px',
-          marginTop: '0px',
-          width: '102%',
-          height: '100%'
-      })
-      $(this).animate({
-          marginLeft: '0px',
-          marginTop: '0px',
-          width: '100%',
-          height: '100px',
-          opacity: '1',
-          zIndex: '999',
-          transition: 'easeOut'
-      }, {
-          duration: 200,
-          complete: function() {
-              $('.littletitle').css({
-                  marginTop: '-50px'
-              });
-              $('.form-pop-lePerche').animate({
-                  height: '100%',
-                  paddingTop: '50px',
-                  opacity: '1',
-                  zIndex: '998'
-              }, 'slow');
 
-          }
-      });
-      $('.tile').not(this).css({
-          display: 'none'
-      });
-
-
-
-
-        smallMovePercheTile();
-    });
-
-}
-// Fin fonction  Aggrandissement tuile "installez-vous dans le Perche"
-
-
-
-//Rétrecissement tuile "installez-vous dans le Perche"
-function smallMovePercheTile() {
-  $('.close-button').click(function() {
-      $('body').css({
-          padding: '',
-          overflowX: 'visible'
-      });
-      $('.container').css({
-          marginLeft: '',
-          marginRight: '',
-          marginTop: '',
-          width: '',
-          height: ''
-      });
-      $('.form-pop-lePerche').animate({
-          height: '150px'
-      }, {
-          duration: 200,
-          complete: function() {
-              $('.form-pop-lePerche').css({
-                  opacity: '0',
-              });
-              $('.movePerche').animate({
-                  height: '250px',
-                  width:'700px'
-              }, {
-                  duration: 400,
-                  complete: function() {
-                      $('.tile').show();
-                  }
-              });
-
-          }
-      });
-        bigMovePercheTile();
-  });
-
-}
-// Fin fonction  Rétrécissement tuile "installez-vous dans le Perche"
+    }
+    // Fin fonction  Rétrécissement tuile "installez-vous dans le Perche"
 
 
 });
