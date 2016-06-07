054ff8f41807d3b798bbdd2f229597fc73e47089
JulienKeita <julien.keita@gmail.com>
2016-06-07 18:01:21 +0200

    07/06_18:01

--- app/models/contact.js
+++ app/models/contact.js
@@ -23,8 +23,30 @@ var Contact = {
       console.log(req.body);
         Contact.model.create(req.body,
             function(err, data) {
+            console.log(data);
               if (!err) {
-                console.log(data);
+                var nodemailer = require('nodemailer');
+                var transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'poleperche28@gmail.com',
+                        pass: 'siap2828'
+                    }
+                });
+                var mailOptions = {
+                    from: 'poleperche28@gmail.com',
+                    to: data.contactEmail,
+                    subject: 'Bienvenue dans le Perche !',
+                    html: 'Bonjour ' + data.contactFirstname + ', <p>Vous avez récemment rencontré un élu du Pôle Perche. Si vous êtes porteur de projet ou si vous souhaitez simplement vous installer dans la région, nous vous invitons à contacter nos conseillers en les appelant au 02 37 29 09 29 ou par mail à paysperche.sia@wanadoo.fr</p> <p> À bientot !</p> <p>L\'équipe du Pôle Perche</p>'
+                };
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        return console.log(error);
+                    }
+                    console.log('Message sent: ' + info.response);
+                });
+
+                transporter.close();
                 res.sendStatus(200);
               } else {
                 console.log(err);
--- app/routes/users.js
+++ app/routes/users.js
@@ -6,30 +6,5 @@ module.exports = function (app) {
     app.post('/users', User.create);
     app.put('/users/:id', User.update);
     app.delete('/users/:id', User.delete);
-    app.post('/toto', function (req, res) {
-        var nodemailer = require('nodemailer');
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'sender@gmail.com',
-                pass: 'password'
-            }
-        });
-        var mailOptions = {
-            from: req.body.sender,
-            to: req.body.destination,
-            subject: req.body.subject,
-            text: req.body.message,
-            html: '<b>' + req.body.message + '</b>'
-        };
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message sent: ' + info.response);
-        });
 
-        transporter.close();
-
-    })
 }
