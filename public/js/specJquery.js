64028335dabb208f797c7c186d4e9a0c16b9d26e
Kikucheeseful <gregorybizet@gmail.com>
2016-06-08 09:31:38 +0200

    gogo Chacha

--- public/js/specJquery.js
+++ public/js/specJquery.js
@@ -23,48 +23,7 @@ $(window).load(function() {
 
     function biggerCreationTile() {
         $('.theCreation').click(function() {
-            $('.tilev').not('.thePhoneMail, .theAgenda').animate({
-                width: '0',
-                height: '0',
-                opacity: '0',
-                marginLeft: '50%',
-                transition: '0.2s ease-out'
-            }, {
-                duration: 200,
-                complete: function() {
-                    $(this).fadeOut(10)
-                }
-            }).animate({
-                backgroundColor: 'green'
-            }, {
-                duration: 100,
-                complete: function() {
-                    $('.theCreation').fadeIn(30).css({
-                        marginLeft: '350px',
-                        width: '0px',
-                        height: '0px',
-                        opacity: '0'
-                    }).animate({
-                        backgroundColor: 'green'
-                    }, {
-                        duration: 70,
-                        complete: function() {
-                            $(this).animate({
-                                opacity: '1',
-                                marginLeft: '0px',
-                                width: '750px',
-                                height: '785px',
-                                transition: '0.2s ease-out'
-                            }, {
-                                duration: 200,
-                                complete: function() {
-                                    $('.closeButton1').css('opacity', '1');
-                                }
-                            });
-                        }
-                    }).addClass("bigCreation").removeClass('theCreation');
-                }
-            });
+
         });
         smallerCreationTile();
     }
@@ -75,50 +34,7 @@ $(window).load(function() {
     ///////////////////////////////////////////////////////////////
     function smallerCreationTile() {
         $('.closeButton').click(function() {
-            $('.tilev').not('.thePhoneMail, .theAgenda').animate({
-                width: '0px',
-                height: '0px',
-                opacity: '0',
-                marginLeft: '350px',
-                transition: '0.2s ease-out'
-            }, {
-                duration: 200,
-                complete: function() {
-                    $(this).fadeOut(10)
-                }
-            }).animate({
-                color: 'green'
-            }, {
-                duration: 100,
-                complete: function() {
-                    $('.bigCreation').fadeIn(30).css({
-                        marginLeft: '350px',
-                        width: '',
-                        height: '',
-                        opacity: '0'
-                    }).animate({
-                        color: 'green'
-                    }, {
-                        duration: 70,
-                        complete: function() {
-                            $('.bigCreation').css({
-                                opacity: '0',
-                                marginLeft: '0px',
-                                width: '350px',
-                                height: '250px',
-                                transition: '0.2s ease-out'
-                            }).fadeIn(10);
-                            $('.tilev').fadeIn(10).animate({
-                                width: '350px',
-                                height: '250px',
-                                opacity: '1',
-                                marginLeft: '100px',
-                                transition: '0.2s ease-out'
-                            }).addClass("theCreation").removeClass('bigCreation');
-                        }
-                    });
-                }
-            });
+            
         });
 
     }
