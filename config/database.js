94b0efb8805ae8b3b2f54a55920a54ce805e315d
JulienKeita <julien.keita@gmail.com>
2016-06-24 15:27:15 +0200

    Edit config for heroku

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 // DATABASE CONFIG
 var mongoose = require('mongoose');
-module.exports = mongoose.connect('mongodb://localhost/Perche-Entreprendre');
+module.exports = mongoose.connect(process.en.MONGODB_URI || 'mongodb://localhost/Perche-Entreprendre');
 
